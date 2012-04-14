956b541bf7c0836c55b2818f10f15469ed56c667
mfakane <star@glasscore.net>
2012-04-14 19:40:03 +0900

    Angle に演算子追加

--- Metasequoia.Sharp/Structs/Angle.cs
+++ Metasequoia.Sharp/Structs/Angle.cs
@@ -11,6 +11,31 @@
 			this.Bank = bank;
 		}
 
+		public static Angle operator +(Angle a, Angle b)
+		{
+			return new Angle(a.Head + b.Head, a.Pitch + b.Bank, a.Bank + b.Bank);
+		}
+
+		public static Angle operator -(Angle a, Angle b)
+		{
+			return new Angle(a.Head - b.Head, a.Pitch - b.Bank, a.Bank - b.Bank);
+		}
+
+		public static Angle operator *(Angle a, float b)
+		{
+			return new Angle(a.Head * b, a.Pitch * b, a.Bank * b);
+		}
+
+		public static Angle operator /(Angle a, float b)
+		{
+			return new Angle(a.Head / b, a.Pitch / b, a.Bank / b);
+		}
+
+		public static Angle operator -(Angle self)
+		{
+			return new Angle(-self.Head, -self.Pitch, -self.Bank);
+		}
+
 		public override string ToString()
 		{
 			return string.Format("{{H:{0} P:{1} B:{2}}}", this.Head, this.Pitch, this.Bank);
